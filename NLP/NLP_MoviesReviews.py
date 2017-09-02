7a1716be82cbc4c62a2f4acca16f20ea2e7c812e
Lama <lamahamadeh1985@hotmail.com>
2017-09-02 10:11:17 +0100

    Update NLP_MoviesReviews.py

--- NLP/NLP_MoviesReviews.py
+++ NLP/NLP_MoviesReviews.py
@@ -107,18 +107,31 @@ print(build_bag_of_words_features_filtered(romeo_words))
 all_words = movie_reviews.words()
 print(len(all_words)/1e6)
 
-filtered_words = [word for word in movie_reviews.words() if not word in useless_words]
+filtered_words = [word for word in movie_reviews.words() 
+                    if not word in useless_words]
 
 print(len(filtered_words)/1e6)
 
+
 from collections import Counter
-
 word_counter = Counter(filtered_words)
-
 most_common_words = word_counter.most_common()[:10] #this method accesses the 
 #words with the highest count.
-
 print(most_common_words)
+#A Counter is a dict subclass for counting hashable objects. It is an unordered
+# collection where elements are stored as dictionary keys and their counts are 
+#stored as dictionary values.
+#Example of counter:
+#cnt = Counter()
+#for word in ['red', 'blue', 'red', 'green', 'blue', 'blue']:
+#    cnt[word] += 1
+#print(cnt)
+#Counter({'blue': 3, 'red': 2, 'green': 1})
+#print(cnt.most_common)
+#[('blue', 3), ('red', 2), ('green', 1)]
+#most_common method returns a list of the n most common elements and their 
+#counts from the most common to the least.
+
 
 import matplotlib.pyplot as plt
 
@@ -146,20 +159,45 @@ plt.hist(sorted_word_counts, bins=50, log=True)
 plt.title('Semilogarithmic Histogram')
 #--------------------
 
-#Naive Bayes Classifier
+#Naive Bayes Classifier(sentiment analysis)
 
 #One of the simplest supervised machine learning classifiers is the Naive Bayes
 # Classifier
 
+#Using our build_bag_of_words_features function we can build separately the 
+#negative and positive features. Basically for each of the 1000 negative and 
+#for the 1000 positive review, we create one dictionary of the words and we 
+#associate the label "neg" and "pos" to it.
 
+negative_features = [(build_bag_of_words_features_filtered(movie_reviews.words(fileids = [f])),'neg')
+    for f in negative_fileids]
+print(negative_features)#1000 records
 
+positive_features = [(build_bag_of_words_features_filtered(movie_reviews.words(fileids = [f])), 'pos')
+    for f in positive_fileids]
+print(positive_features)#1000 records
 
+from nltk.classify import NaiveBayesClassifier
 
+#Train the model
+split = 800 #we take 80% of the data for training
+sentiment_classifier = NaiveBayesClassifier.train(positive_features[:split]+
+                                                  negative_features[:split])
 
+#check the accuracy of the model
+print(nltk.classify.util.accuracy(sentiment_classifier, positive_features[:split]+
+                            negative_features[:split])*100)
+#98.06
 
+#predicting the labels for the rest of 20% of the data and check the accuracy 
+#of the testing on the remaining data
+print(nltk.classify.util.accuracy(sentiment_classifier, positive_features[split:]+
+                            negative_features[split:])*100)
+#71.75
 
-
-
-
+#We can finally print the most informative features, i.e. the words that mostly
+# identify a positive or a negative review
+print(sentiment_classifier.show_most_informative_features())
 
 #--------------------
+
